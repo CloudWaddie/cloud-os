1593736b52809664ce6722b393f981818845f7e0
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-11-23 16:23:00 +1030

    alternate!!

--- boot.asm
+++ boot.asm
@@ -3,8 +3,18 @@ mov al, 'A'
 int 0x10
 loop:
     inc al
-    cmp al, 'Z' + 1
-    je exit
+    cmp al, 'Z'
+    jg nocaps
+    jmp caps
+caps:
+    mov ah, 0x0e
+    int 0x10
+    jmp loop
+nocaps:
+    cmp al, 'Z'
+    jg exit
+    mov ah, 0x0e
+    sub al, 32
     int 0x10
     jmp loop
 exit:
