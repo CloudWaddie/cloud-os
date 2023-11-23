a08d89c4035e14aa9c32a316be8014e45fdbbb34
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-11-23 16:14:47 +1030

    Fix loop in boot.asm to print uppercase letters

--- boot.asm
+++ boot.asm
@@ -1,7 +1,13 @@
 mov ah, 0x0e
-mov al, 65
+mov al, 'A'
 int 0x10
-inc al
-
+loop:
+    inc al
+    cmp al, 'Z' + 1
+    je exit
+    int 0x10
+    jmp loop
+exit:
+    jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
